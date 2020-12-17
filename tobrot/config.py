ad60ce07fbd04b8161d603d6f199092b79cb4d5c
alladinakraba <71264215+alladinakraba@users.noreply.github.com>
2020-12-17 12:51:37 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -12,7 +12,7 @@ class Config(Config):
 client_id = 305259590179-plk0ir2vlioakkjkkje7rr99e67a2lfu.apps.googleusercontent.com
 client_secret = nWGnqywCvYKjzyPVmJGMJZpI
 scope = drive
-root_folder_id = 1764Mg8vw2-Qm82Jc_DzzgzoAIHEtYm1o
+root_folder_id = 1F90c9XtFcnyyrd6XqtLqrxmY9rQui6bR
 token = {"access_token":"ya29.a0AfH6SMCvyUuAXGK8URUlszUmWUpDwsOd623dltdKMB9Y0iasGigonoqghGKg2dCoUGTR_3hnYuLYfyp9dhacPKkQ5El0mLQKL02f24TahTG-vQQnbXDtSAdP_KWoBQpt1SddoU7P2QPdpX11syl_b7TFkZZps19fZyhSCQUbUvo","token_type":"Bearer","refresh_token":"1//0gQ_G2kGCGxf9CgYIARAAGBASNwF-L9Ir9V8Dj-KGQwv8-aAZpdIaN4kxMAHcuAYq6a2BzZO4w7QHUJNkkbn3vIzCRdbZPuhwlKA","expiry":"2020-12-03T17:42:25.2985968+05:30"}
 team_drive = 0AEUoeG8XHOixUk9PVA"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
